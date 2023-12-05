707c117b1cf2e4105795ad813ec1d8b00b1e666a
niyacv27 <146043722+niyacv27@users.noreply.github.com>
2023-12-05 12:19:06 +0530

    Update prims.c

--- prims algorithm.c/prims.c
+++ prims algorithm.c/prims.c
@@ -44,7 +44,7 @@ else
 cost[i][j]=G[i][j];
 spanning[i][j]=0;
 }
-//initialise visited[],distance[] and from[]
+
 distance[0]=0;
 visited[0]=1;
 for(i=1;i<n;i++)
@@ -53,11 +53,11 @@ distance[i]=cost[0][i];
 from[i]=0;
 visited[i]=0;
 }
-min_cost=0; //cost of spanning tree
-no_of_edges=n-1; //no. of edges to be added
+min_cost=0; 
+no_of_edges=n-1; 
 while(no_of_edges>0)
 {
-//find the vertex at minimum distance from the tree
+
 min_distance=infinity;
 for(i=1;i<n;i++)
 if(visited[i]==0&&distance[i]<min_distance)
@@ -66,12 +66,12 @@ v=i;
 min_distance=distance[i];
 }
 u=from[v];
-//insert the edge in spanning tree
+
 spanning[u][v]=distance[v];
 spanning[v][u]=distance[v];
 no_of_edges--;
 visited[v]=1;
-//updated the distance[] array
+
 for(i=1;i<n;i++)
 if(visited[i]==0&&cost[i][v]<distance[i])
 {
